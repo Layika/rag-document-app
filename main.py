bda33d6afa56ceec49a096cde2a2a651685c49cc
Layika <zulayikanalukenge@gmail.com>
2026-04-19 20:35:53 +0300

    app setup

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-def main():
-    print("Hello from document-rag-app!")
+from fastapi import FastAPI
+from pydantic import BaseModel
+import os
+from openai import OpenAI
 
+app = FastAPI()
 
-if __name__ == "__main__":
-    main()
+class ChatRequest(BaseModel):
+    message: str
+
+@app.get("/")
+def home():
+    return {"message": "hello"}
